364e0ef6b836d7229f50389dd792c3d520cab174
Fadilou-coder <syfadilou3@gmail.com>
2021-08-31 10:35:30 +0000

    first commit

---
+++ src/main/java/com/odc/suiviapprenants/SuiviapprenantsApplication.java
@@ -0,0 +1,13 @@
+package com.odc.suiviapprenants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviapprenantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviapprenantsApplication.class, args);
+	}
+
+}
