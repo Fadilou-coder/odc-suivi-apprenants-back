e785d100fd04d97fac6d928b3df99182a7102200
Fadilou-coder <syfadilou3@gmail.com>
2021-09-08 10:38:17 +0000

    updating gitignore

--- src/main/java/com/odc/suiviapprenants/utils/JwtUtil.java
+++ src/main/java/com/odc/suiviapprenants/utils/JwtUtil.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 @Service
 public class JwtUtil {
 
-  private String SECRET_KEY = "secret";
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
