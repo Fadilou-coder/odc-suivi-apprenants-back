e0c8825505f5eca08f751f5896313dc70ab4db96
Omar faye <omar.fayeomzolive@gmail.com>
2021-11-09 22:27:17 +0000

    git

---
+++ src/main/java/com/odc/suiviapprenants/dataFixture/ApprenantFixture.java
@@ -0,0 +1,34 @@
+package com.odc.suiviapprenants.dataFixture;
+
+import com.odc.suiviapprenants.model.Apprenant;
+import com.odc.suiviapprenants.repository.ApprenantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@Component
+@ConditionalOnProperty(name = "app.db-init", havingValue = "true")
+@Order(3)
+public class ApprenantFixture implements CommandLineRunner {
+    private ApprenantRepository apprenantRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String password = "password";
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+
+        for (int i = 0; i < 50; i++) {
+            apprenantRepository.save(
+                    new Apprenant("apprenant"+i, encodedPassword, "apprenant_prenom"+i, "apprenant_nom"+i, "apprenant@gmail.com"+i, "1 254 5678 901"+i, "adresse"+i, "7700000"+i, "EN COURS", "DEVWEB2020-12920001", "APPRENANT")
+            );
+        }
+    }
+}
