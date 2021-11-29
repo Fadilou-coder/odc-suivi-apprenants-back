62c8886b1064785cc25ab23966dff4aa26491cd0
abdou-karim <abdoukarimsidibe1@gmail.com>
2021-11-29 11:47:39 +0000

    get all formateur

--- src/main/java/com/odc/suiviapprenants/repository/FormateurRepository.java
+++ src/main/java/com/odc/suiviapprenants/repository/FormateurRepository.java
@@ -3,12 +3,13 @@ package com.odc.suiviapprenants.repository;
 import com.odc.suiviapprenants.model.Formateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FormateurRepository extends JpaRepository<Formateur,Long> {
 
     Optional<Formateur> findByUsernameAndArchiveFalse(String username);
-
+    List<Formateur> findAllByArchiveFalse();
     Optional<Formateur> findByIdAndArchiveFalse(Long id);
     Optional<Formateur> findByUsername(String username);
 }
