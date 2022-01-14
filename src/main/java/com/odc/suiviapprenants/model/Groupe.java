d54e7be27ee4cf32ab425eb976db1cb41b1fe06a
Fadilou-coder <syfadilou3@gmail.com>
2022-01-14 17:10:30 +0000

    assigner apprenant à brief

--- src/main/java/com/odc/suiviapprenants/model/Groupe.java
+++ src/main/java/com/odc/suiviapprenants/model/Groupe.java
@@ -18,8 +18,8 @@ public class Groupe extends AbstractEntity {
     private String type;
     private String statut;
 
-     @ManyToMany
-    /*@ManyToMany(cascade = CascadeType.PERSIST)*/
+     /*@ManyToMany*/
+    @ManyToMany(cascade = CascadeType.PERSIST)
     private Collection<Apprenant> apprenants;
 
     @ManyToMany
